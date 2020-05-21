9720490d37025ccf56d09a48b74dadb4f7c426af
Maxofil <maxcze18@gmail.com>
2020-05-21 22:04:57 +0200

    js v2

--- assets/genericDropdown.js
+++ assets/genericDropdown.js
@@ -1,5 +1,9 @@
-refreshPlugins.push(function(el) {
-    $(el).find('select[name ="genericDropdown"]').change(function (event) {
-        $(this).closest('form').netteAjax(event);
-    });
-});
+(function ($) {
+    $.fn.genericDropdown = function (options) {
+        return (function() {
+            $(el).find('select[name ="genericDropdown"]').change(function (event) {
+                $(this).closest('form').netteAjax(event);
+            });
+        });
+    }
+})(jQuery);
